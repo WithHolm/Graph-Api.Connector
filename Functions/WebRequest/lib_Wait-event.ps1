bc64e1daac328d22e571cbcb5d17654286ac4502
Philip Meholm <philip.meholm@gmail.com>
2017-09-04 20:38:41 +0200

    dd

---
+++ Functions/WebRequest/lib_Wait-event.ps1
@@ -0,0 +1,46 @@
+Function Wait-Event
+{
+    param(
+    #Output from Get-EventSubscriber
+    [parameter(
+        ValueFromPipeline=$true,
+        ParameterSetName='EventsubPipeline')]
+    [System.Management.Automation.PSEventSubscriber]$Eventsub,
+
+    #manual search for EventSubscriber
+    [parameter(
+        mandatory=$true,
+        ParameterSetName='Manual')]
+    [String]$name,
+
+    [int]$WaitMaxSec = 8 
+    )
+
+    begin
+    {
+        $SW = [System.Diagnostics.Stopwatch]::new()
+        
+        if($PsCmdlet.ParameterSetName -eq "Manual")
+        {
+            return $(Get-EventSubscriber|where{$_.Action.name -like "$name"}|Wait-Event)
+        }
+    }
+    process
+    {
+        $SW.Start()
+        foreach($event in $Eventsub)
+        {
+            #$event.Action.Name
+            Write-verbose "Waiting for event '$($event.Action.Name)' to finish"
+            while((Get-EventSubscriber|where{$_.SourceIdentifier -like $event.SourceIdentifier}).Action.State -eq "NotStarted" )
+            {       
+                Start-Sleep -Milliseconds 100
+                if($SW.ElapsedMilliseconds -gt ($WaitMaxSec * 1000))
+                {
+                    throw "waiting for $($event.name) timed out after $maxwaitsec seconds"
+                }
+            }
+            (Get-EventSubscriber|where{$_.SourceIdentifier -like $event.name})
+        }
+    }
+}
